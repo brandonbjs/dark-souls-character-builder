958ae43c910b92946403038c17b46e623c551381
Brandon James Shaffer <bjs397@nau.edu>
2023-08-23 04:04:00 -0700

    Created package.json for my souls-builder-api. Updated connect.js and CharBuilderAPI.js and made a successful connection to my MongoDB.

--- souls-builder-api/CharBuilderAPI.js
+++ souls-builder-api/CharBuilderAPI.js
@@ -1,1 +1,25 @@
-// TODO: write API with endpoints
+import express from "express"; 
+import cors from "cors";
+import "./connect.js";
+// import { addPlayer, fetchPlayers, fetchTeam, findPlayerById, updatePlayer, deletePlayer, resetPlayers } from "./Services/DraftToolService.js";
+
+const app = express(); 
+
+app.use(express.json());
+app.use(cors());
+
+
+// TODO: write endpoints for API
+
+
+
+
+
+//sets variable PORT to the port number we are using, 1234
+let PORT = 1234;
+
+//opens the PORT and listening for requests - logs this information
+//uses function() callback function that will execute once the server starts listening on the specified PORT.
+app.listen(PORT, function(){
+    console.log("Listening at port:", PORT);
+})
--- souls-builder-api/connect.js
+++ souls-builder-api/connect.js
@@ -1,1 +1,21 @@
-// TODO: connect to MongoDB
+import mongoose from "mongoose";
+
+const serverName = "127.0.0.1";
+
+const databaseName = "DarkSoulsCharacterBuilder"
+
+const URL = `mongodb://${serverName}:27017/${databaseName}`;
+
+class Database{
+    constructor(){
+        this._connect();
+    }
+
+    _connect(){
+        mongoose.connect(URL)
+            .then(()=>console.log("database connection established"))
+            .catch(err=>console.log("error occured", err))
+    }
+}
+
+export default new Database();
