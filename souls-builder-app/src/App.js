2f14a8a676c258cd32aa4054eb4195f8da07be63
Brandon James Shaffer <bjs397@nau.edu>
2023-12-18 00:27:46 -0800

    Added the first two dropdown select fields inside the CharacterBuilder.js class component. Also added two buttons to allow for a New Build to be created and a build to be Reset. All four features are within the attributes-grid. Created two custom arropw methods (handleNewBuildClick and handleResetClick) which are event handlers for our newly added buttons. Styled everything with a 'Dark Souls' theme and positioned it responsively and correctly. Included a cool bonfire gif with credits at the bottom of the page.

--- souls-builder-app/src/App.js
+++ souls-builder-app/src/App.js
@@ -5,12 +5,29 @@ import CharacterBuilder from './components/CharacterBuilder'
 
 class App extends Component {
   
+  // render our two components and 
   render() {
     return (
       <div className="App">
-        <NavigationBar />
-        <CharacterBuilder />
+        
+        <div className='navigationBar'>
+          <NavigationBar />
+        </div>
+
+        <div className='characterBuilder'>
+          <h2>Dark Souls Character Builder</h2>
+          <CharacterBuilder />
+        </div>
+        
+        <div className='bonfire'>
+          <img src="/dark-souls-character-builder/souls-builder-app/images/dark-souls-bonfire.gif"
+            alt="Animated Bonfire by tRoedder (@https://trmrddr.tumblr.com/)" 
+            style={{ display: 'block', margin: 'auto' }} />
+        </div>
+        
       </div>
+
+      
     );
   }
 }
--- souls-builder-app/src/components/CharacterBuilder.js
+++ souls-builder-app/src/components/CharacterBuilder.js
@@ -1,11 +1,64 @@
 import React, { Component } from 'react'
 
 class CharacterBuilder extends Component {
+
+  handleNewBuildClick = () => {
+    // Open a new page in another tab
+    window.open('http://localhost:3000/', '_blank');
+  };
+
+  handleResetClick = () => {
+    // Refresh the current page
+    window.location.reload();
+  };
+
   render() {
     return (
+      /* character-builder div encompasses our three columns (attributes-grid,
+         items-grid, and stats-grid) and their resprective grids/fields
+         
+         attributes-grid will house fields and inputs for: class, gender, level, 
+         and all attribute points (based on what class is chosen): all starting classes 
+         in Dark Souls begin with varying levels ***which affects the amount of attributes 
+         they start with*** */
       <div className="character-builder">
+        
         <div className="attributes-grid">
-          {/* attributes components go here */}
+          <div className="dropdown">
+            <label htmlFor="characterClass">Class</label>
+            <select id="characterClass" className='characterClass'>
+              <option value="knight">Warrior</option>
+              <option value="mage">Knight</option>
+              <option value="mage">Wanderer</option>
+              <option value="mage">Thief</option>
+              <option value="mage">Bandit</option>
+              <option value="mage">Hunter</option>
+              <option value="mage">Sorcerer</option>
+              <option value="mage">Pyromancer</option>
+              <option value="mage">Cleric</option>
+              <option value="mage">Deprived</option>
+            </select>
+
+            <button type="button" className='newBuildButton' 
+                    onClick={this.handleNewBuildClick}>
+                New Build
+            </button>
+
+          </div>
+
+          <div className="dropdown">
+            <label htmlFor="characterGender">Gender</label>
+            <select id="characterGender" className='characterGender'>
+              <option value="male">Male</option>
+              <option value="female">Female</option>
+            </select>
+
+            <button type="button" className='resetButton' 
+                    onClick={this.handleResetClick}>
+              Reset
+            </button>
+          </div>
+
         </div>
 
         <div className="items-grid">
